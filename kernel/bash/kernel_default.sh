76e9d2823f1cd98646be43b84f0df7f1f2d508c0
Ricardo Pardini <ricardo@pardini.net>
2024-04-12 16:31:24 +0200

    introduce TEMPLATE, LINUXKIT_VERSION, USE_KERNEL_ID

--- kernel/bash/kernel_default.sh
+++ kernel/bash/kernel_default.sh
@@ -13,10 +13,12 @@ function calculate_kernel_version_default() {
 		exit 1
 	fi
 
-	# The default kernel output id is just the arch, unless KCONFIG is not generic
+	# The default kernel output id is just the arch, unless KCONFIG is not generic, or USE_KERNEL_ID is set
 	declare -g OUTPUT_ID="${ARCH}"
 	if [[ "${KCONFIG}" != "generic" ]]; then
 		OUTPUT_ID="${KCONFIG}-${KERNEL_MAJOR}.${KERNEL_MINOR}.y-${ARCH}"
+	elif [[ -n "${USE_KERNEL_ID}" ]]; then
+		OUTPUT_ID="${kernel_id}"
 	fi
 
 	# Calculate the KERNEL_ARCH from ARCH; also what is the cross-compiler package needed for the arch
@@ -47,7 +49,13 @@ function calculate_kernel_version_default() {
 	input_hash="$(cat "kernel/configs/${INPUT_DEFCONFIG}" "kernel/Dockerfile" | sha256sum - | cut -d ' ' -f 1)"
 	short_input_hash="${input_hash:0:8}"
 	kernel_oci_version="${KERNEL_MAJOR}.${KERNEL_MINOR}.${KERNEL_POINT_RELEASE}-${short_input_hash}"
-	kernel_oci_image="${HOOK_KERNEL_OCI_BASE}${kernel_id}:${kernel_oci_version}"
+
+	kernel_id_to_use="${kernel_id}"
+	if [[ -n "${USE_KERNEL_ID}" ]]; then
+		kernel_id_to_use="${USE_KERNEL_ID}"
+	fi
+
+	kernel_oci_image="${HOOK_KERNEL_OCI_BASE}${kernel_id_to_use}:${kernel_oci_version}"
 
 	# Log the obtained version & images to stderr
 	log info "Kernel arch: ${KERNEL_ARCH} (for ARCH ${ARCH})"
